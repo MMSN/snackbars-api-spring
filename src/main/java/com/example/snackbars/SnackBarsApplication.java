17f75ee86a300c4e4a09e57e73ddd33066b7b0f1
Mateus Nascimento <mateus.msnascimento@gmail.com>
2022-12-05 11:23:22 -0300

    chore: begin project

---
+++ src/main/java/com/example/snackbars/SnackBarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.snackbars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackBarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackBarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snackbars/SnackBarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snackbars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackBarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
